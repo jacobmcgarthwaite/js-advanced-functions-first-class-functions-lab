e919ef0ccbc09034125762c198f496931c2d239f
Jacob <jacobliem1@gmail.com>
2021-04-08 13:18:23 -0500

    commit tree 777c21e57be77d8d433ccc4567b3a7a675388fda

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+ const returnFirstTwoDrivers = function (){
+     const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+     firstTwoDrivers = drivers[0][1];
+ }
